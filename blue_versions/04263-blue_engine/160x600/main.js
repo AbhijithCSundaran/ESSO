9183347b719ba5bfb8a2b36da1d42b15c21db281
AbhijithCSundaran <abhijith.sundaran@annalect.com>
2024-06-10 18:49:09 +0530

    300x250

--- blue_versions/04263-blue_engine/160x600/main.js
+++ blue_versions/04263-blue_engine/160x600/main.js
@@ -84,11 +84,12 @@ function animate() {
   tl.to(".shadow", 4, { autoAlpha: 0.4, ease: Power2.easeInOut }, "-=4")
   tl.to(".copy1", 0.5, { autoAlpha: 1, ease: Power2.easeInOut }, "-=4")
 
-  // tl.to(".shadow", 4, {autoAlpha: 0.6, ease: Power2.easeInOut}, "+=1.5")
-  // tl.to(".phone", 4, { top: 136, ease: Power2.easeInOut })
   tl.to(".copy1", 0.5, { autoAlpha: 0, ease: Power2.easeInOut }, "+=1.5")
   tl.to(".copy2", 0.5, { autoAlpha: 1, ease: Power2.easeInOut })
   tl.to("#cta", 0.5, { autoAlpha: 1, ease: Power2.easeInOut }, "+=0.5")
+  
+  tl.to(".phone", 4, { top: 136, ease: Power2.easeInOut },"-=4")
+  tl.to(".shadow", 4, {autoAlpha: 0.6, ease: Power2.easeInOut}, "-=4")
 
 
   moveY(phone, -1);
